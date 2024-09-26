2a922c64545a2b96d20f45914d54fb234ef4ef70
muqtasid87 <muqtasid87@gmail.com>
2024-09-26 19:47:25 +0800

    yes

--- src/components/save_production_model.py
+++ src/components/save_production_model.py
@@ -7,7 +7,7 @@ def download_model():
     production = run.use_artifact('muqtasid87-international-islamic-university-malaysia-org/wandb-registry-model/toll_plaza:production', type='model')
     production_dir = production.download(root=f"production_model")
     model_file = f"{production_dir}/best.pt"
-    # shutil.move(model_file, f"{production_dir}/model_{production.version}.pt")
+    shutil.move(model_file, f"{production_dir}/model_{production.version}.pt")
     wandb.finish()
     print(f"Model saved to {production_dir}")
 

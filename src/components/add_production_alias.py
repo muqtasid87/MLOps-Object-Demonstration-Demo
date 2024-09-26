f18bf40775c30b51222484c63f0908f8e8be44c6
muqtasid87 <muqtasid87@gmail.com>
2024-09-26 19:34:10 +0800

    did soemthing idk

--- src/components/add_production_alias.py
+++ src/components/add_production_alias.py
@@ -3,10 +3,11 @@ import wandb
 
 def run_deployment():
     wandb.init(anonymous='must')
-    if metrics_latest.box.map > metrics_production.box.map:
+    if metrics_latest.box.map >= metrics_production.box.map:
         latest = wandb.use_artifact('muqtasid87-international-islamic-university-malaysia-org/wandb-registry-model/toll_plaza:latest', type='model')
         latest.aliases.append("production")
         latest.save()
+        print("Production alias added to latest model")
 
     wandb.finish()
     

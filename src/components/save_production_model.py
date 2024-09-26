91e1f6fa527f61e999b9b23b4a396a07d726d6cc
muqtasid87 <muqtasid87@gmail.com>
2024-09-26 18:41:46 +0800

    updated renaming model

--- src/components/save_production_model.py
+++ src/components/save_production_model.py
@@ -1,12 +1,12 @@
 import wandb
-import os
+import shutil
 run = wandb.init(name="git_download")
 
 #Download latest trained model
 production = run.use_artifact('muqtasid87-international-islamic-university-malaysia-org/wandb-registry-model/toll_plaza:production', type='model')
 production_dir = production.download(root=f"model/production")
-model_file = f"{production_dir}/best.py"
-os.rename(model_file, f"{production.version}_production_model")
+model_file = f"{production_dir}\\best.pt"
+shutil.move(model_file, f"{production_dir}/{production.version}_production_model.pt")
 
 
 

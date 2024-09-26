54caee331daa93577351bf76989780a3c633b0fa
muqtasid87 <muqtasid87@gmail.com>
2024-09-26 18:34:15 +0800

    fixed typo

--- src/components/save_production_model.py
+++ src/components/save_production_model.py
@@ -4,7 +4,7 @@ run = wandb.init(name="git_download")
 
 #Download latest trained model
 production = run.use_artifact('muqtasid87-international-islamic-university-malaysia-org/wandb-registry-model/toll_plaza:production', type='model')
-production_dir = production.download(root=f"model/production/")
+production_dir = production.download(root=f"model/production")
 model_file = f"{production_dir}/best.py"
 os.rename(model_file, f"{production.version}_production_model")
 
